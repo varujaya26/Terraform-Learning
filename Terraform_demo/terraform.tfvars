2c1f972551245479e93bdd39614f3f82c332d525
varujaya26 <varujayapal26@gmail.com>
2023-10-07 21:00:38 +0530

    Terraform demo

---
+++ Terraform_demo/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value = "ami-053b0d53c279acc90"
+instance_type_value = "t2.micro"
+subnet_id_value = "subnet-0a4b98893054228f7"
+key_name_value = "t11"
---
+++ Terraform_demo/variables.tf
@@ -0,0 +1,18 @@
+variable "ami_value" {
+    description = "Value of the AMI"
+  
+}
+
+variable "instance_type_value" {
+    description = "Instance type of the EC2"
+  
+}
+
+variable "subnet_id_value" {
+    description = "Subnet_id of the EC2"
+}
+
+variable "key_name_value" {
+    description = "Key pair of the EC2"
+  
+}
