8b797faa274f8b3f1a35b0f1a634ca4e27d0892b
varujaya26 <varujayapal26@gmail.com>
2023-10-08 12:59:23 +0530

    module-example-terraform

--- Terraform_demo/main.tf
+++ Terraform_demo/main.tf
@@ -2,9 +2,10 @@ provider "aws" {
    region = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
-     ami                     = var.ami_value
-     instance_type           = var.instance_type_value
-     subnet_id               = var.subnet_id_value
-     key_name                = var.key_name_value
+module "ec2_instance" {
+  source = "./module/ec2_instance"
+  ami_value = "ami-053b0d53c279acc90"
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-0a4b98893054228f7"
+  key_name_value = "t11"
 }
